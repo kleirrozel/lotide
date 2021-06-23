0a8d620ba8bb9c412ff74d50eb7d09554ff54548
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-22 23:07:53 -0400

    Edited without function using push method

--- without.js
+++ without.js
@@ -17,19 +17,17 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`❌❌❌NOT Equal: ${array1} !== ${array2}`);
   }
 };
-//TO DO: This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
-const without = function(source, itemsToRemove) {
 
-  for (let i = source.length - 1; i >= 0; i--) {
-    for (let x = 0; x < itemsToRemove.length; x++) {
-      if (source[i] === itemsToRemove[x]) {
-        source.splice(i, 1)
-      }
+const without = function(array1, array2) {
+  let itemsToKeep = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      itemsToKeep.push(array1[i]);
     }
   }
-  console.log(source);
-  return source;
-};
+  return itemsToKeep;
+}
 
 console.log(without([1, 2, 3], [1])) // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
