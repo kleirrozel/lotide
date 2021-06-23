4749a1c6390243ac019866b9e40c829b3a6bc623
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-22 20:43:22 -0400

    Commit for implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// const eqArrays = function(a, b){
+//   return Array.isArray(a) && Array.isArray(b) && a.length === b.length && a.every((val, index) => val === b[index])
+//};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let value = 0; value < array1.length; value ++) {
+    if (array1[value] !== array2[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// // TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
