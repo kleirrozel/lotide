9baf6b45bd397a3ad8644b4ec1d3962fb25c5791
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-22 21:16:42 -0400

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let value = 0; value < array1.length; value ++) {
+    if (array1[value] !== array2[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) { 
+    console.log(`✅✅✅Equal: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌NOT Equal: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1,3,4],[2,3,1]);
+assertArraysEqual([1,3,4],[1,3,4]);
