b049b8c015fb478e7c9e8e687a62d485db3f3150
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-24 17:18:55 -0400

    Commit for function where you scan the object and return the first key or which the callback returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TO DO: Take in an object and a callback. 
+// Scan the object and return the first key or which the callback returns a truthy value
+// If no key is found, return undefined
+
+const findKey = function(object, callback) {
+  
+  for (const key in object) {
+  if (callback(object[key]) === true) {
+    return key;
+    }
+  }
+};
+
+// Test
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma" - should pass
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "elvisbun"); // => "Blue Hill" - should fail
