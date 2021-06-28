98e65c6971752ce9580b70d251d62fddf9dc47db
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 19:46:41 -0400

    Incorporated mocha and chai to eqArrays test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe("#eqArrays", () => {
+  it("returns true when [1, 2, 3] and [1, 2, 3] are compared", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false when [1, 2, 3] and [3, 2, 1] are compared", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns true when ['1', '2', '3'] and ['1', '2', '3'] are compared", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  it("returns false when ['1', '2', '3'], ['1', '2', 3] are compared", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+});
