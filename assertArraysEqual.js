7a292c48239a5fa69ccc0c2d026683014773d948
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 14:38:28 -0400

    Refactored assertArraysEqual with module exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value ++) {
-    if (array1[value] !== array2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) { 
@@ -18,5 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1,3,4],[2,3,1]);
-assertArraysEqual([1,3,4],[1,3,4]);
+module.exports = assertArraysEqual;
