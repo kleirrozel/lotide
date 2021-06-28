e6c30388fe2632f72021c8d256b068f52d943fe7
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 15:05:28 -0400

    Test file for middle function

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
