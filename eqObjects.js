799ae4578958c70fc529dd818556f9ab4e836ea6
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 20:20:46 -0400

    Refactored eqObjects with module exports

--- eqObjects.js
+++ eqObjects.js
@@ -1,12 +1,4 @@
-// The function eqObjects will take in two objects and returns true or false, based on a perfect match.
-// Meaning the objects have identical keys with identical values
-// Otherwise, false
-
-// First, use the Object.keys function on both objects and compare their lengths. If not equal, there's no point in going further: return false
-// After that check, loop through the keys returned by Object.keys for one of the objects (not both). Use for..of since the keys are an array. 
-// Inside our loop, compare both objects' values for that key. Use === to ensure that the types are the same!
-// As soon as there is not a match, we can return false
-// Our control flow will therefore only get to the end of the loop if all the keys matched. We can and should thus return true at the end (after the loop)
+const eqArrays = require ("./eqArrays");
 
 const eqObjects = function(object1, object2) {
   let objKey1 = Object.keys(object1);
@@ -30,21 +22,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value ++) {
-    if (array1[value] !== array2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+module.exports = eqObjects;
