0496bae1d7ad8692c35215f295bc352ea5268951
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 20:50:46 -0400

    Refactored without with module exports

--- without.js
+++ without.js
@@ -1,22 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value ++) {
-    if (array1[value] !== array2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) { 
-    console.log(`✅✅✅Equal: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌NOT Equal: ${array1} !== ${array2}`);
-  }
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
 const without = function(array1, array2) {
   let itemsToKeep = [];
@@ -29,10 +11,4 @@ const without = function(array1, array2) {
   return itemsToKeep;
 }
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
