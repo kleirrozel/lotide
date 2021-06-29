a72719d0ac15cc2e46b8e746dad392228cb75336
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 21:53:01 -0400

    Refactored letterPositions with module exports

--- letterPositions.js
+++ letterPositions.js
@@ -1,32 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value ++) {
-    if (array1[value] !== array2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) { 
-    console.log(`✅✅✅Equal: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌NOT Equal: ${array1} !== ${array2}`);
-  }
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TO DO: Return all the indices (zero-based positions) in the string where each character is found.
+const assertArraysEqual = require("./assertArraysEqual");
 
 const letterPositions = function(sentence) {
   const result = {};
@@ -41,6 +13,4 @@ const letterPositions = function(sentence) {
   return result;
 };
 
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("lighthouse in the house").t, [3]); // => should say not equal
-assertArraysEqual(letterPositions("elvisbun").b, [5]);
+module.exports = letterPositions;
