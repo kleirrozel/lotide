bbddbea10c0d85b74a0e7e81565e4e7d13919dff
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 21:33:29 -0400

    Refactored countOnly with module exports

--- countOnly.js
+++ countOnly.js
@@ -1,16 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// countOnly function needs to report back how many instances of each string were found in the allItems array of strings (what strings and their count)
-// Needs to return an object containing counts of everything that the input object listed
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+const assertEqual = require("./assertEqual");
 
 const countOnly = function(allItems, itemsToCount){ 
   let results = {};
@@ -27,21 +15,4 @@ const countOnly = function(allItems, itemsToCount){
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
