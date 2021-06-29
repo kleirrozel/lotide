997c3d47710eda710842fb49edc18a8f7e257838
Kleir Miranda <kleirmiranda@gmail.com>
2021-06-28 21:42:51 -0400

    Refactored findKey with module exports

--- findKey.js
+++ findKey.js
@@ -1,14 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TO DO: Take in an object and a callback. 
-// Scan the object and return the first key or which the callback returns a truthy value
-// If no key is found, return undefined
+const assertEqual = require("./assertEqual");
 
 const findKey = function(object, callback) {
   
@@ -19,21 +9,4 @@ const findKey = function(object, callback) {
   }
 };
 
-// Test
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"); // => "noma" - should pass
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 1), "elvisbun"); // => "Blue Hill" - should fail
+module.exports = findKey;
